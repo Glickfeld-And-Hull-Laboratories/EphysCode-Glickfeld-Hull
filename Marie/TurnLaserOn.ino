2b09fb57cf09e86de16b39d140b7f55ecf7a48a2
mariehemelt <70349432+mariehemelt@users.noreply.github.com>
2025-02-27 16:12:07 -0500

    Add files via upload

---
+++ Marie/TurnLaserOn.ino
@@ -0,0 +1,14 @@
+const int ledPin = 10;       // the pin that the LED is attached to
+
+
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+
+}
