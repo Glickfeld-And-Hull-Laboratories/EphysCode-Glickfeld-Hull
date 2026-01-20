f2ea575573e16d020a9083fcfdbdeeafda8530e3
ArdentRodent <tingweijerryhu@gmail.com>
2026-01-20 10:25:40 -0500

    setting up jerry ephys repo

---
+++ jerry/runCatGTret_TH.m
@@ -0,0 +1,77 @@
+% 
+% runCatGT_SG runs two commands that pull the timestamps of events from the 
+% nidaq stream (stimulus on events) and the imec stream (chassis pulse
+% aligned to spike times)
+%
+% Inputs:
+%   date    = date of experiment
+% Outputs:
+%   catgt_ folder
+%
+% Outputs located in Analysis/Neuropixel/date folder.
+%
+
+function runCatGTret_SG(date)
+
+% Set base directories
+    dataDir     = '\\duhs-user-nc1.dhe.duke.edu\dusom_glickfeldlab\All_staff\home\sara\Data\neuropixel\';
+    analysisDir = '\\duhs-user-nc1.dhe.duke.edu\dusom_glickfeldlab\All_staff\home\sara\Analysis\Neuropixel\';
+
+% Construct full data path
+    fullDataPath = fullfile(dataDir, date);
+
+% Get list of folders in the specified date directory
+    dirContents = dir(fullDataPath);
+    runFolders  = {dirContents([dirContents.isdir] & ~ismember({dirContents.name}, {'.', '..'})).name};
+
+% Check if exactly one folder is found
+    if isempty(runFolders)
+        error('No run folder found in %s', fullDataPath);
+    elseif numel(runFolders) > 1
+        error('  *runCatGT_SG* Multiple run folders found in %s. Please check the data and experiment structure.', fullDataPath);
+    end
+
+% Get full run folder and name
+    runFolder = fullfile(fullDataPath, runFolders{1});
+
+% Get run name minus experimental run digits
+    runName = runFolders{1};
+    runName = regexprep(runName, '(_imec0)$', '');    % Remove _imec0 ending on file name
+    runName = regexprep(runName, '(_[gt]\d+)$', '');    % Remove '_gN' or '_tN' at the end if present
+        % _ matches underscore
+        % [gt] matches either g or t
+        % \d+ matches one or more digits (i.e., any number after g or t)
+        % $ ensures this pattern is only removed if it's at the end of
+        % runNAme
+    runName = regexprep(runName, '(_[gt]\d+)$', '');    % Run twice, in case there is _gN_tN
+
+% Extract gN and tN from the .bin filename
+    binFiles = dir(fullfile(runFolder, '*.imec0.ap.bin'));  % Locate .bin file inside runFolder
+    if isempty(binFiles)
+        error('No .bin file found in %s', runFolder);
+    elseif numel(binFiles) > 1
+        error('Multiple .bin files found in %s. Check data folder.', runFolder);
+    end
+    binFileName = binFiles(1).name;
+    exptdigits  = regexp(binFileName, '_g(\d+)_t(\d+)', 'tokens');   % Find digits associated with gN and tN
+    if isempty(exptdigits)
+        error('Could not extract gN and tN from %s', binFileName);
+    end    
+    gNum = exptdigits{1}{1}; % Extract gN
+    tNum = exptdigits{1}{2}; % Extract tN
+
+% Construct terminal commands with detected gNum and tNum
+    cmd1 = sprintf('CatGT -dir=%s -run=%s -g=%s -t=%s -prb=0 -ni -xd=0,0,0,0,0 -xd=0,0,0,1,0 -xd=0,0,0,2,0 -xd=0,0,0,5,0 -dest=%s', fullDataPath, runName, gNum, tNum, fullfile(analysisDir, date));   % Pull nidaq channel timestamps
+        % channel 0 is chassis
+        % channel 1 is mworks stim on signal
+        % channel 2 is multiclamp protocol
+        % channel 5 is photodiode
+    cmd2 = sprintf('CatGT -dir=%s -run=%s -g=%s -t=%s -prb=0 -ap -xd=2,0,-1,6,500 -no_auto_sync -dest=%s', fullDataPath, runName, gNum, tNum, fullfile(analysisDir, date));    % Pull neural data sync pulse timestamps
+        % We expect it to be 500 ms long, on the last (i.e., -1) channel and on bit 6 (7th input) of that channel
+
+% Execute commands
+    cd('C:\Users\smg92\Desktop\CatGTWinApp4.3\CatGT-win');
+    system(cmd1);
+    system(cmd2);
+
+end
---
+++ jerry/runTPrimeRet_TH.m
@@ -0,0 +1,61 @@
+% 
+% runTPrime_SG syncs the times of events on nidaq to spike times.
+%
+% Inputs:
+%   date    = date of experiment
+% Outputs:
+%   _mworksStimOnSync.txt
+%   _photodiodeSync.txt
+%
+% Outputs located in Analysis/Neuropixel/date folder.
+%
+
+function runTPrimeRet_SG(date)
+
+% Set base directories
+    dataDir     = '\\duhs-user-nc1.dhe.duke.edu\dusom_glickfeldlab\All_staff\home\sara\Data\neuropixel\';
+    analysisDir = '\\duhs-user-nc1.dhe.duke.edu\dusom_glickfeldlab\All_staff\home\sara\Analysis\Neuropixel\';
+
+% Construct full data path
+    mkdir(['\\duhs-user-nc1.dhe.duke.edu\dusom_glickfeldlab\All_Staff\home\sara\Analysis\Neuropixel\' date '\retinotopy'])
+    fullAnalysisPath    = fullfile(analysisDir, date);
+
+% Get list of folders in the specified date directory
+    dirContents   = dir(fullAnalysisPath);
+    catgtFolders  = {dirContents([dirContents.isdir] & startsWith({dirContents.name}, 'catgt') & contains({dirContents.name}, 'retinotopy')).name};
+
+% Check if exactly one catgt folder is found
+    if isempty(catgtFolders)
+        error('No catgt folder found in %s', fullAnalysisPath);
+    elseif numel(catgtFolders) > 1
+        error('*runTPrime_SG* Multiple catgt folders found in %s. Check analysis folder.', fullAnalysisPath);
+    end
+
+% Use the detected catgt folder
+    catgtFolder = fullfile(fullAnalysisPath, catgtFolders{1});
+
+% Extract run name from catgt folder
+    runName = regexprep(catgtFolders{1}, '^catgt_', ''); % Remove 'catgt_' prefix
+
+% Construct paths for TPrime
+    baseFileName        = fullfile(catgtFolder, [runName, '_tcat']);
+    spikeSyncFile       = [baseFileName, '.imec0.ap.xd_384_6_500.txt'];
+    nidaqSync           = [baseFileName, '.nidq.xd_0_0_500.txt'];   % ndiaq clock signal
+    nidaqMWevents       = [baseFileName, '.nidq.xd_0_1_0.txt']; % mworks stim on events
+    nidaqPDevents       = [baseFileName, '.nidq.xd_0_5_0.txt']; % photodiode events
+    mworksStimOnSync    = fullfile(fullAnalysisPath, '\retinotopy', [date, '_mworksStimOnSync.txt']);   % Output file for MWorks stim on signal
+    photodiodeSync      = fullfile(fullAnalysisPath, '\retinotopy',[date, '_photodiodeSync.txt']);   % Output file for photodiode stim on signal
+
+% Construct the TPrime command
+    cmd = sprintf(['TPrime -syncperiod=1.000000 ' '-tostream=%s ' '-fromstream=1,%s ' '-events=1,%s,%s ' '-events=1,%s,%s'], spikeSyncFile, nidaqSync, nidaqMWevents, mworksStimOnSync, nidaqPDevents, photodiodeSync);
+
+% Execute the command in the Windows terminal
+    cd('C:\Users\smg92\Desktop\TPrime-win');
+    system(cmd);
+
+end
+
+
+
+
+
